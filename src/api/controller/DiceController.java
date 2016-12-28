13e600c821e909e189ba64193adfc2de05f4d24a
Peter Lorentzen <peter.lorentzen@ec.se>
2016-12-28 13:33:04 +0100

    add log4j

--- src/api/controller/DiceController.java
+++ src/api/controller/DiceController.java
@@ -6,6 +6,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import org.apache.log4j.Logger;
 import org.json.JSONException;
 
 import api.controllerservice.DiceControllerService;
@@ -15,9 +16,12 @@ import api.model.DiceSet;
 @Path("/Dice")
 public class DiceController extends ControllerBase {
 
+    final static Logger logger = Logger.getLogger(DiceController.class);
+
     @GET
     @Produces("application/json")
     public Response dice() throws JSONException {
+        logger.debug("Get 1 Dice");
         Dice dice = DiceControllerService.GetDice();
         return OkResponse(dice);
     }
@@ -26,7 +30,8 @@ public class DiceController extends ControllerBase {
     @GET
     @Produces("application/json")
     public Response diceSet() throws JSONException {
-        DiceSet ds = DiceControllerService.GetDiceSet(5);        
+        logger.debug("Get 1 set of 5 Dice");
+        DiceSet ds = DiceControllerService.GetDiceSet(5);
         return OkResponse(ds);
     }
 
@@ -34,14 +39,12 @@ public class DiceController extends ControllerBase {
     @GET
     @Produces("application/json")
     public Response diceSet(@PathParam("amount") int f) throws JSONException {
-        
-        if(f <= 0){
+        if (f <= 0) {
+            logger.info(f + "is less then 0");
             return BadRequestResponse("amount has to be greater then 0");
         }
-        
+        logger.debug("Get 1 set of " + f + " Dice");
         DiceSet ds = DiceControllerService.GetDiceSet(f);
         return OkResponse(ds);
     }
-
-    
 }
